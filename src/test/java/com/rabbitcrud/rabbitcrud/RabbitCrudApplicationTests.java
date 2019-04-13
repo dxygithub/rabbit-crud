564babba6b95cab99f8960d27b4ddd777a5142e9
dxygithub <389228699@qq.com>
2019-04-13 11:50:52 +0800

    测试

--- src/test/java/com/rabbitcrud/rabbitcrud/RabbitCrudApplicationTests.java
+++ src/test/java/com/rabbitcrud/rabbitcrud/RabbitCrudApplicationTests.java
@@ -110,6 +110,7 @@ public class RabbitCrudApplicationTests {
 
     public static void main(String[] args) {
         System.out.println("duxiaoyu");
+        System.out.println("wangtao");
     }
 
 }
