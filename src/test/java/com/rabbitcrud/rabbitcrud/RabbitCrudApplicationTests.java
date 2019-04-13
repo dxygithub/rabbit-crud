8de5aa475cc5e65570110fda4d2c8d441899537e
dxygithub <389228699@qq.com>
2019-04-13 17:23:52 +0800

    测试

--- src/test/java/com/rabbitcrud/rabbitcrud/RabbitCrudApplicationTests.java
+++ src/test/java/com/rabbitcrud/rabbitcrud/RabbitCrudApplicationTests.java
@@ -111,6 +111,7 @@ public class RabbitCrudApplicationTests {
     public static void main(String[] args) {
         System.out.println("王涛");
         System.out.println("杜晓宇");
+        System.out.println("测试");
     }
 
 }
